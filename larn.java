db92eb5ff1abb7aaec041d64d3c2fc0804f2be7c
Vvlado3 <72707803+vvlado3@users.noreply.github.com>
2021-03-09 10:28:38 +0200

    Add files via upload

---
+++ larn.java
@@ -0,0 +1,10 @@
+package javat;
+import java.util.Scanner;
+public class larn {
+	public static void main(String args[]){
+		double b = 252;
+		double a = 2.54;
+		System.out.print(b * a);
+		
+    }
+}
