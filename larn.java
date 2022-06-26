3fe03d4ce1b2ae7adf02772d317c4fb05652b9d6
vvlado3 <72707803+vvlado3@users.noreply.github.com>
2022-06-27 01:23:58 +0300

    Update larn.java

--- larn.java
+++ larn.java
@@ -1,10 +1,11 @@
-package javat;
-import java.util.Scanner;
-public class larn {
-	public static void main(String args[]){
-		double b = 252;
-		double a = 2.54;
-		System.out.print(b * a);
-		
+public class Loop {
+    public static void main(String[] args) {
+        loop1:
+        for (int i = 0; i < 5; i++) {
+            for (int k = 0; k < 5; k++) {
+                if (i == 3)
+                    break loop1;
+                System.out.println("i = " + i + " j = " + j);
+            }
+        }
     }
-}
